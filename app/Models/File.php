74388d39af5ff8444ac3dd44eaaee9417447f926
Mário Costa <mariocostaa6@gmail.com>
2025-12-05 10:34:43 +0000

    database: create files table and model

---
+++ app/Models/File.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'original_name',
+        'path',
+        'mime_type',
+        'size',
+        'download_count',
+        'expires_at',
+    ];
+
+    protected $casts = [
+        'expires_at' => 'datetime',
+    ];
+
+    public function isExpired(): bool
+    {
+        return $this->expires_at && $this->expires_at->isPast();
+    }
+}
